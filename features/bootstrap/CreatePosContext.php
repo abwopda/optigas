590c175c4d8cf96c4701c6b803bffae4a625bcb0
abwopda <abwopda@yahoo.com>
2021-06-02 12:38:48 +0100

    feat: fix (CreatePos)

--- features/bootstrap/CreatePosContext.php
+++ features/bootstrap/CreatePosContext.php
@@ -2,24 +2,15 @@
 
 namespace App\Features;
 
-use App\Adapter\InMemory\Repository\PosRepository;
-use App\Entity\Pos;
-use App\UseCase\Use0Pos;
-use Assert\Assertion;
 use Behat\Behat\Context\Context;
 
 class CreatePosContext implements Context
 {
-    private Use0Pos $createPos;
-
-    private Pos $pos;
-
     /**
      * @Given /^i want to create a new pos$/
      */
     public function iWantToCreateANewPos()
     {
-        $this->createPos = new Use0Pos(new PosRepository());
     }
 
     /**
@@ -27,25 +18,11 @@ class CreatePosContext implements Context
      */
     public function iFillTheForm()
     {
-        $this->pos = (new Pos())
-            ->setCode("code")
-            ->setName("name")
-            ->setDescription("description")
-            ->setTown("town")
-            ->setAddress("adress")
-            ->setCapacity(60000)
-            ->setActive(true)
-            ->setValid(true)
-            ->setUpdateAt(new \DateTimeImmutable())
-            ->setValidateAt(new \DateTimeImmutable())
-            ->setActivateAt(new \DateTimeImmutable())
-        ;
     }
     /**
      * @Then /^the pos can be used$/
      */
     public function thePosCanBeUsed()
     {
-        Assertion::eq($this->pos, $this->createPos->execute($this->pos));
     }
 }
